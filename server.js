7ee75ae2a0d8bde7addec95474cfc8d6903aa9a1
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-08-26 12:31:08 -0400

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
